813e84b9676efe575328114968284acace88ebdc
Swayam Sarthak Swain <swayamsarthakswain018@gmail.com>
2023-12-18 23:45:38 +0530

    sidebar completed

--- src/components/LeftSideBar.tsx
+++ src/components/LeftSideBar.tsx
@@ -1,30 +1,59 @@
-import React from 'react'
-import { BiHomeCircle } from 'react-icons/bi';
-import { HiOutlineHashtag } from 'react-icons/hi';
+import React from "react";
+import { BiHomeCircle } from "react-icons/bi";
+import { HiOutlineHashtag } from "react-icons/hi";
 import { GrNotification } from "react-icons/gr";
+import { IoSearchOutline } from "react-icons/io5";
+import { MdMailOutline } from "react-icons/md";
+// import { RiFileList2Line } from "react-icons/ri";
+import { IoPeopleOutline } from "react-icons/io5";
+import { IoPeopleSharp } from "react-icons/io5"; //clicked value
+import { FaXTwitter } from "react-icons/fa6";
+import { FaRegUser } from "react-icons/fa6";
+import { FaUser } from "react-icons/fa6";
+import { CgMoreO } from "react-icons/cg";
+import { FiMoreHorizontal } from "react-icons/fi";
+import { LuSquareEqual } from "react-icons/lu";
+import { BsSlashSquare } from "react-icons/bs";
 type TItem = {
-    title:string,
-    icon:string
-}
+  title: string;
+  icon: string;
+};
 
-const NavigationItems = [{ title: "Home", icon: BiHomeCircle },
- { title: "Explore", icon: GrNotification },
-  { title: "Notifications", icon: BiHomeCircle },
-   {title:"Messages", icon: HiOutlineHashtag},
-{title:"Bookmarks", icon: HiOutlineHashtag},{title:"Twiiter Blue", icon: HiOutlineHashtag},{title:"Profile", icon: HiOutlineHashtag},{title:"More", icon:HiOutlineHashtag}]
+const NavigationItems = [
+    {title:"Twitter",icon:FaXTwitter},
+  { title: "Home", icon: BiHomeCircle },
+  { title: "Explore", icon: IoSearchOutline },
+  { title: "Notifications", icon: GrNotification },
+  { title: "Messages", icon: MdMailOutline },
+  { title: "Grok", icon: BsSlashSquare },
+  { title: "List", icon: LuSquareEqual },
+  { title: "Communities", icon: IoPeopleOutline },
+  { title: "Premium", icon: FaXTwitter },
+  { title: "Profile", icon: FaRegUser },
+  { title: "More", icon: CgMoreO },
+];
 
 function LeftSideBar() {
-    return (
-        <aside className=' w-72 h-screen flex flex-col space-y-2 bg-gray-500'>
-            {NavigationItems.map((items:any,index)=>(
+  return (
+    <aside className=" w-72 h-full flex flex-col bg-black space-y-2">
+        <div>
 
-                <div key={index} className='flex flex-row space-x-2 items-center bg-slate-500 hover:bg-black px-4 py-2 text-xl rounded-full mx-4'>
-                    <items.icon/>
-                    <span>{items.title}</span>
-                </div>
-            ))}
-        </aside>
-    )
+      {NavigationItems.map((items: any, index) => (
+        <div
+          key={index}
+          className="flex flex-row w-fit gap-5 items-center hover:bg-gray-700/40 py-[10px] text-2xl  rounded-full mx-4 font-semibold justify-start px-5"
+        >
+          <div>
+             <items.icon />
+          </div>
+          {items.title==="Twitter"?"":<div>{items.title}</div>}
+        </div>
+      ))}
+        </div>
+      <button type="button" className=" bg-primary py-[10px] text-2xl mx-4 w-52 rounded-full">Post</button>
+
+    </aside>
+  );
 }
 
-export default LeftSideBar
+export default LeftSideBar;
